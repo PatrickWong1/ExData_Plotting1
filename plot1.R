502d95835444654470ec5d6ade3f2c5a8825ca57
PatrickWong1 <patrick.t.c.wong@gmail.com>
2014-05-10 21:15:29 -0700

    plot1.R

--- plot1.R
+++ plot1.R
@@ -2,10 +2,10 @@
 
 data<-read.table("household_power_consumption.txt", header=TRUE, sep=";")
 data$Date<-as.Date(data$Date, format="%d/%m/%Y")    #convert date in categorical factor to R date
-%data$Time<-strptime(data$Time, format="%H:%M:%S")   #convert Time in categorical factor to R time
+#data$Time<-strptime(data$Time, format="%H:%M:%S")   #convert Time in categorical factor to R time
 
-data.subsetted<-subset(data, data$Date=="2007-2-1" | data$Date=="2007-2-2") %subset 2 days
-data.subsetted$gapkw<-as.numeric(as.character(data.subsetted$Global_active_power)) %convert factor to char to numeric
+data.subsetted<-subset(data, data$Date=="2007-2-1" | data$Date=="2007-2-2") #subset 2 days
+data.subsetted$gapkw<-as.numeric(as.character(data.subsetted$Global_active_power)) #convert factor to char to numeric
 #hist(data.subsetted$gapkw, col="red", main= "Global Active Power", xlab= "Global Active POwer (kilowatts)", xaxp  = c(0, 1200, 6),yaxp= c(0,6,3) )
 hist(data.subsetted$gapkw, col="red", main= "Global Active Power", xlab= "Global Active POwer (kilowatts)" )
 dev.copy(png, file = "plot1.png", width=480, height=480)  ## Copy my plot to a PNG file
